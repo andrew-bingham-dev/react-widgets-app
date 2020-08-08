fe919524e2898704708d9e556d74f6cfbec9b47a
andrew-bingham-dev <andrew.bingham.dev@gmail.com>
2020-08-08 13:14:06 +0100

    initial commit of app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
-function App() {
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a JavaScript framework',
+  },
+  {
+    title: 'Why use React?',
+    content: 'To easily get a fun job paying lots of money',
+  },
+  {
+    title: 'How do you use React?',
+    content: 'Very well, thank you!',
+  },
+];
+
+const options = [
+  {
+    label: 'The colour red',
+    value: 'red',
+  },
+  {
+    label: 'The colour green',
+    value: 'green',
+  },
+  {
+    label: 'The colour blue',
+    value: 'blue',
+  },
+];
+
+const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Route path='/'>
+        <Accordion items={items} />
+      </Route>
+      <Route path='/list'>
+        <Search />
+      </Route>
+      <Route path='/dropdown'>
+        <Dropdown
+          label='Select a colour'
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path='/translate'>
+        <Translate />
+      </Route>
     </div>
   );
-}
+};
 
 export default App;
